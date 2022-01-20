ebcbc4b09a60b90c116a9a0d4d60b660107e5cfd
Yowko Tsai <yowko.tsai@skylinetw.com>
2022-01-20 10:54:56 +0800

    use IConfiguration

---
+++ ArrayConfig/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+var configs= app.Configuration.GetSection("ConfigArray").Get<string[]>();
+
+foreach (var config in configs)
+{
+    Console.WriteLine(config);
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
